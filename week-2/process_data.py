9052fb500d7503a3bc42b6dcc41cf571250e5fd4
kayvane1 <kayvane.shakerifar@gmail.com>
2022-07-11 12:13:46 +0100

    updates for sweep runs

--- week-2/process_data.py
+++ week-2/process_data.py
@@ -11,7 +11,7 @@ from transformers import AutoTokenizer
 default_cfg = config_dict.ConfigDict()
 
 # WANDB BASE PARAMETERS
-default_cfg.PROJECT_NAME = 'wandb-week-2-complaints-classifier'
+default_cfg.PROJECT_NAME = "wandb-week-2-complaints-classifier"
 # WANDB ARTIFACT TYPES
 default_cfg.DATASET_TYPE = "dataset"
 default_cfg.MODEL_TYPE = "model"
@@ -25,10 +25,10 @@ default_cfg.PROCESSED_DATA_ARTIFACT = "complaints_processed_data"
 default_cfg.TRAIN_DATA_ARTIFACT = "complaints_train_data"
 default_cfg.TEST_DATA_ARTIFACT = "complaints_test_data"
 # DATA FOLDERS
-default_cfg.RAW_DATA_FOLDER = 'complaints-dataset/raw'
-default_cfg.PROCESSED_DATA_FOLDER = 'complaints-dataset/processed'
-default_cfg.TRAIN_DATA_FOLDER = 'complaints-dataset/train'
-default_cfg.TEST_DATA_FOLDER = 'complaints-dataset/test'
+default_cfg.RAW_DATA_FOLDER = "complaints-dataset/raw"
+default_cfg.PROCESSED_DATA_FOLDER = "complaints-dataset/processed"
+default_cfg.TRAIN_DATA_FOLDER = "complaints-dataset/train"
+default_cfg.TEST_DATA_FOLDER = "complaints-dataset/test"
 # DATASET COLUMNS TO KEEP
 default_cfg.dataset = "consumer-finance-complaints"
 default_cfg.text_column = "Complaint Text"
@@ -37,14 +37,38 @@ default_cfg.split_perc = 10
 # TRANSFORMERS PARAMETERS
 default_cfg.model_name = "distilbert-base-uncased"
 
+
 def parse_args():
     "Overriding default argments"
-    argparser = argparse.ArgumentParser(description='Prepare Data for Complaints Allocation')
-    argparser.add_argument('--dataset', type=str, default=default_cfg.dataset, help='Name of the Dataset on the Huggingface Hub')
-    argparser.add_argument('--text_column', type=str, default=default_cfg.text_column, help='Column in the dataset whichc ontains the text data / complaint background and narrative')
-    argparser.add_argument('--target_column', type=str, default=default_cfg.target_column, help='Column in the dataset which should be used to as the target for predictions')
-    argparser.add_argument('--split_perc', type=int, default=default_cfg.split_perc, help='percentage of the dataset to used')
+    argparser = argparse.ArgumentParser(
+        description="Prepare Data for Complaints Allocation"
+    )
+    argparser.add_argument(
+        "--dataset",
+        type=str,
+        default=default_cfg.dataset,
+        help="Name of the Dataset on the Huggingface Hub",
+    )
+    argparser.add_argument(
+        "--text_column",
+        type=str,
+        default=default_cfg.text_column,
+        help="Column in the dataset whichc ontains the text data / complaint background and narrative",
+    )
+    argparser.add_argument(
+        "--target_column",
+        type=str,
+        default=default_cfg.target_column,
+        help="Column in the dataset which should be used to as the target for predictions",
+    )
+    argparser.add_argument(
+        "--split_perc",
+        type=int,
+        default=default_cfg.split_perc,
+        help="percentage of the dataset to used",
+    )
     return argparser.parse_args()
+
 
 def log_raw_data(cfg):
     """
@@ -52,21 +76,25 @@ def log_raw_data(cfg):
 
     Args
         cfg (ConfigDict): ConfigDict object containing the configuration for the pipeline
-    
+
     Returns:
         None
-    
+
     """
 
-    with wandb.init(project=cfg.PROJECT_NAME, job_type=cfg.RAW_DATA_JOB_TYPE, config=dict(cfg)) as run:
-        
+    with wandb.init(
+        project=cfg.PROJECT_NAME, job_type=cfg.RAW_DATA_JOB_TYPE, config=dict(cfg)
+    ) as run:
+
         cfg = wandb.config
 
         if cfg.split_perc is not None:
             split_percentage = f"train[:{cfg.split_perc}%]"
-        
+
         # Loading consumer complaints dataset - Note: This is a big dataset
-        text_dataset = load_dataset(default_cfg.dataset, split=split_percentage, ignore_verifications=True)
+        text_dataset = load_dataset(
+            default_cfg.dataset, split=split_percentage, ignore_verifications=True
+        )
         text_dataset.save_to_disk(cfg.RAW_DATA_FOLDER)
         # Create and log the raw data artifact
         raw_data_art = wandb.Artifact(cfg.RAW_DATA_ARTIFACT, type=cfg.DATASET_TYPE)
@@ -77,31 +105,36 @@ def log_raw_data(cfg):
 def process_and_log_data(cfg):
     """
     Pulls the raw data from W&B and processes it into a dataset for training and testing
-    
+
     Args:
         cfg (ConfigDict): ConfigDict object containing the configuration for the pipeline
-    
+
     Returns:
         None
     """
 
-    with wandb.init(project=cfg.PROJECT_NAME, job_type=cfg.DATA_PROCESSING_JOB_TYPE, config=dict(cfg)) as run:
+    with wandb.init(
+        project=cfg.PROJECT_NAME,
+        job_type=cfg.DATA_PROCESSING_JOB_TYPE,
+        config=dict(cfg),
+    ) as run:
 
         cfg = wandb.config
 
         tokenizer = AutoTokenizer.from_pretrained(cfg.model_name)
 
         # By including `use_artifact` we're logging the usage to W&B and can track it as part of the lineage
-        text_artifact = run.use_artifact(f'{cfg.RAW_DATA_ARTIFACT}:latest')
+        text_artifact = run.use_artifact(f"{cfg.RAW_DATA_ARTIFACT}:latest")
         _ = text_artifact.download(root=cfg.RAW_DATA_FOLDER)
         text_dataset = load_from_disk(cfg.RAW_DATA_FOLDER)
-
 
         # Extracting the target column, there is only one split at this point (train)
         columns = text_dataset.column_names
 
         # Remove the columns which aren't in scope for us
-        remove_cols = [e for e in columns if e not in (cfg.text_column, cfg.target_column)]
+        remove_cols = [
+            e for e in columns if e not in (cfg.text_column, cfg.target_column)
+        ]
         processed_data = text_dataset.remove_columns(remove_cols)
 
         # Renaming the columns to the names expected by the classifier
@@ -109,11 +142,13 @@ def process_and_log_data(cfg):
         processed_data = processed_data.rename_column(cfg.target_column, "labels")
 
         # Filtering out empty/no-text complaints
-        processed_data = processed_data.filter(lambda example: len(example['text'])>0)
+        processed_data = processed_data.filter(lambda example: len(example["text"]) > 0)
 
         processed_data.save_to_disk(cfg.PROCESSED_DATA_FOLDER)
         # Create and log the raw data artifact
-        processed_data_art = wandb.Artifact(cfg.PROCESSED_DATA_ARTIFACT, type=cfg.DATASET_TYPE)
+        processed_data_art = wandb.Artifact(
+            cfg.PROCESSED_DATA_ARTIFACT, type=cfg.DATASET_TYPE
+        )
         processed_data_art.add_dir(cfg.PROCESSED_DATA_FOLDER)
         run.log_artifact(processed_data_art)
 
@@ -124,28 +159,30 @@ def split_and_log_data(cfg):
 
     Args:
         cfg (ConfigDict): ConfigDict object containing the configuration for the pipeline.
-    
+
     Returns:
         None
-    
+
     """
 
-    with wandb.init(project=cfg.PROJECT_NAME, job_type=cfg.SPLIT_DATA_JOB_TYPE, config=dict(cfg)) as run:
-        
+    with wandb.init(
+        project=cfg.PROJECT_NAME, job_type=cfg.SPLIT_DATA_JOB_TYPE, config=dict(cfg)
+    ) as run:
+
         cfg = wandb.config
 
         # By including `use_artifact` we're logging the usage to W&B and can track it as part of the lineage
-        processed_artifact = run.use_artifact(f'{cfg.PROCESSED_DATA_ARTIFACT}:latest')
+        processed_artifact = run.use_artifact(f"{cfg.PROCESSED_DATA_ARTIFACT}:latest")
         _ = processed_artifact.download(root=cfg.PROCESSED_DATA_FOLDER)
         processed_data = load_from_disk(cfg.PROCESSED_DATA_FOLDER)
 
         # Splitting the dataset into training and validation datasets
-        split_data = processed_data.train_test_split(test_size=0.2,seed=0)
+        split_data = processed_data.train_test_split(test_size=0.2, seed=0)
 
-        train_dataset = split_data['train']
+        train_dataset = split_data["train"]
         train_dataset.save_to_disk(cfg.TRAIN_DATA_FOLDER)
 
-        test_dataset = split_data['test']
+        test_dataset = split_data["test"]
         test_dataset.save_to_disk(cfg.TEST_DATA_FOLDER)
 
         # Create and log the train data artifact
@@ -158,6 +195,7 @@ def split_and_log_data(cfg):
         test_data_art.add_dir(cfg.TEST_DATA_FOLDER)
         run.log_artifact(test_data_art)
 
+
 def run_data_pipeline(cfg):
     """
     Runs the data processing pipeline.
@@ -168,6 +206,6 @@ def run_data_pipeline(cfg):
     split_and_log_data(cfg)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     default_cfg.update(vars(parse_args()))
     run_data_pipeline(default_cfg)
--- week-2/train.py
+++ week-2/train.py
@@ -1,5 +1,12 @@
 import argparse
-from transformers import AutoTokenizer, AutoConfig, TrainingArguments, Trainer, AutoModelForSequenceClassification, DataCollatorWithPadding
+from transformers import (
+    AutoTokenizer,
+    AutoConfig,
+    TrainingArguments,
+    Trainer,
+    AutoModelForSequenceClassification,
+    DataCollatorWithPadding,
+)
 from transformers.trainer_callback import EarlyStoppingCallback
 from datasets import load_metric, load_from_disk
 import numpy as np
@@ -12,7 +19,7 @@ import wandb
 default_cfg = config_dict.ConfigDict()
 
 # WANDB BASE PARAMETERS
-default_cfg.PROJECT_NAME = 'wandb-week-2-complaints-classifier'
+default_cfg.PROJECT_NAME = "wandb-week-2-complaints-classifier"
 # WANDB ARTIFACT TYPES
 default_cfg.DATASET_TYPE = "dataset"
 default_cfg.MODEL_TYPE = "model"
@@ -22,9 +29,9 @@ default_cfg.MODEL_TRAINING_JOB_TYPE = "model-training"
 default_cfg.TRAIN_DATA_ARTIFACT = "complaints_train_data"
 default_cfg.TEST_DATA_ARTIFACT = "complaints_test_data"
 # DATA FOLDERS
-default_cfg.TRAIN_DATA_FOLDER = 'complaints-dataset/train'
-default_cfg.TEST_DATA_FOLDER = 'complaints-dataset/test'
-default_cfg.MODEL_DATA_FOLDER = 'complaints-model'
+default_cfg.TRAIN_DATA_FOLDER = "complaints-dataset/train"
+default_cfg.TEST_DATA_FOLDER = "complaints-dataset/test"
+default_cfg.MODEL_DATA_FOLDER = "complaints-model"
 # TRANSFORMERS PARAMETERS
 default_cfg.model_name = "distilbert-base-uncased"
 default_cfg.num_epochs = 3
@@ -40,94 +47,142 @@ default_cfg.HUB_STRATEGY = "every_save"
 
 def parse_args():
     "Overriding default argments"
-    argparser = argparse.ArgumentParser(description='Process base parameters & hyper-parameters')
-    argparser.add_argument('--model_name', type=str, default=default_cfg.model_name, help='Base Model Architecture to use')
-    argparser.add_argument('--num_epochs', type=int, default=default_cfg.num_epochs, help='number of training epochs')
-    argparser.add_argument('--train_batch_size', type=int, default=default_cfg.train_batch_size, help='train batch size')
-    argparser.add_argument('--eval_batch_size', type=int, default=default_cfg.eval_batch_size, help='eval batch size')
-    argparser.add_argument('--warmup_steps', type=int, default=default_cfg.warmup_steps, help='warmup steps')
-    argparser.add_argument('--learning_rate', type=float, default=default_cfg.learning_rate, help='learning rate to use')
-    argparser.add_argument('--fp16', type=str, default=default_cfg.fp16, help='Whether to use floating point precision')
-    argparser.add_argument('--log_model', action="store_true", help='log best model W&B')
+    argparser = argparse.ArgumentParser(
+        description="Process base parameters & hyper-parameters"
+    )
+    argparser.add_argument(
+        "--model_name",
+        type=str,
+        default=default_cfg.model_name,
+        help="Base Model Architecture to use",
+    )
+    argparser.add_argument(
+        "--num_epochs",
+        type=int,
+        default=default_cfg.num_epochs,
+        help="number of training epochs",
+    )
+    argparser.add_argument(
+        "--train_batch_size",
+        type=int,
+        default=default_cfg.train_batch_size,
+        help="train batch size",
+    )
+    argparser.add_argument(
+        "--eval_batch_size",
+        type=int,
+        default=default_cfg.eval_batch_size,
+        help="eval batch size",
+    )
+    argparser.add_argument(
+        "--warmup_steps",
+        type=int,
+        default=default_cfg.warmup_steps,
+        help="warmup steps",
+    )
+    argparser.add_argument(
+        "--learning_rate",
+        type=float,
+        default=default_cfg.learning_rate,
+        help="learning rate to use",
+    )
+    argparser.add_argument(
+        "--fp16",
+        type=str,
+        default=default_cfg.fp16,
+        help="Whether to use floating point precision",
+    )
+    argparser.add_argument(
+        "--log_model", action="store_true", help="log best model W&B"
+    )
     return argparser.parse_args()
 
 
 def compute_metrics(eval_pred):
-  # define metrics and metrics function
-  f1_metric = load_metric("f1")
-  accuracy_metric = load_metric( "accuracy")
-  recall_metric = load_metric("recall")
-  precision_metric = load_metric("precision")
-  
-  predictions, labels = eval_pred
-  predictions = np.argmax(predictions, axis=1) # predictions.argmax(-1)
-  acc = accuracy_metric.compute(predictions=predictions, references=labels)
-  recall = recall_metric.compute(predictions=predictions, references=labels, average='macro')
-  f1 = f1_metric.compute(predictions=predictions, references=labels, average="macro")
-  precision = precision_metric.compute(predictions=predictions, references=labels, average="macro")
+    # define metrics and metrics function
+    f1_metric = load_metric("f1")
+    accuracy_metric = load_metric("accuracy")
+    recall_metric = load_metric("recall")
+    precision_metric = load_metric("precision")
 
-  return {
-      "accuracy": acc["accuracy"],
-      "f1": f1["f1"],
-      "recall": recall["recall"],
-      "precision" : precision["precision"]
-  }
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)  # predictions.argmax(-1)
+    acc = accuracy_metric.compute(predictions=predictions, references=labels)
+    recall = recall_metric.compute(
+        predictions=predictions, references=labels, average="macro"
+    )
+    f1 = f1_metric.compute(predictions=predictions, references=labels, average="macro")
+    precision = precision_metric.compute(
+        predictions=predictions, references=labels, average="macro"
+    )
+
+    return {
+        "accuracy": acc["accuracy"],
+        "f1": f1["f1"],
+        "recall": recall["recall"],
+        "precision": precision["precision"],
+    }
+
 
 def load_data(run, cfg):
 
-        # By including `use_artifact` we're logging the usage to W&B and can track it as part of the lineage
-        train_artifact = run.use_artifact(f'{cfg.TRAIN_DATA_ARTIFACT}:latest')
-        _ = train_artifact.download(root=cfg.TRAIN_DATA_FOLDER)
-        train_dataset = load_from_disk(cfg.TRAIN_DATA_FOLDER)
+    # By including `use_artifact` we're logging the usage to W&B and can track it as part of the lineage
+    train_artifact = run.use_artifact(f"{cfg.TRAIN_DATA_ARTIFACT}:latest")
+    _ = train_artifact.download(root=cfg.TRAIN_DATA_FOLDER)
+    train_dataset = load_from_disk(cfg.TRAIN_DATA_FOLDER)
 
-        test_dataset = run.use_artifact(f'{cfg.TEST_DATA_ARTIFACT}:latest')
-        _ = test_dataset.download(root=cfg.TEST_DATA_FOLDER)
-        test_dataset = load_from_disk(cfg.TEST_DATA_FOLDER)
-        
-        tokenizer = AutoTokenizer.from_pretrained(cfg.model_name)
+    test_dataset = run.use_artifact(f"{cfg.TEST_DATA_ARTIFACT}:latest")
+    _ = test_dataset.download(root=cfg.TEST_DATA_FOLDER)
+    test_dataset = load_from_disk(cfg.TEST_DATA_FOLDER)
 
-        # tokenizer helper function
-        def _tokenize(batch):
-            return tokenizer(batch["text"], padding='max_length', truncation=True)
+    tokenizer = AutoTokenizer.from_pretrained(cfg.model_name)
 
-        # tokenize dataset
-        train_dataset = train_dataset.map(_tokenize, batched=True)
-        test_dataset = test_dataset.map(_tokenize, batched=True)
+    # tokenizer helper function
+    def _tokenize(batch):
+        return tokenizer(batch["text"], padding="max_length", truncation=True)
 
-        # set format for pytorch
-        train_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
-        test_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
+    # tokenize dataset
+    train_dataset = train_dataset.map(_tokenize, batched=True)
+    test_dataset = test_dataset.map(_tokenize, batched=True)
 
-        return train_dataset, test_dataset
+    # set format for pytorch
+    train_dataset.set_format("torch", columns=["input_ids", "attention_mask", "labels"])
+    test_dataset.set_format("torch", columns=["input_ids", "attention_mask", "labels"])
+
+    return train_dataset, test_dataset
+
+
 def train(cfg):
 
-    with wandb.init(project=cfg.PROJECT_NAME, job_type=cfg.MODEL_TRAINING_JOB_TYPE, config=dict(cfg)) as run:
+    with wandb.init(
+        project=cfg.PROJECT_NAME, job_type=cfg.MODEL_TRAINING_JOB_TYPE, config=dict(cfg)
+    ) as run:
 
         cfg = wandb.config
-    
+
         training_args = TrainingArguments(
-        output_dir=cfg.MODEL_DATA_FOLDER,
-        num_train_epochs=cfg.num_epochs,
-        per_device_train_batch_size=cfg.train_batch_size,
-        per_device_eval_batch_size=cfg.eval_batch_size,
-        warmup_steps=cfg.warmup_steps,
-        fp16=cfg.fp16,
-        learning_rate=float(cfg.learning_rate),
-        # logging & evaluation strategies
-        logging_dir=f"{cfg.MODEL_DATA_FOLDER}/logs",
-        logging_steps=50, 
-        evaluation_strategy="steps",
-        eval_steps=500,
-        save_strategy="steps",
-        save_steps=500,
-        save_total_limit=2,
-        load_best_model_at_end=True,
-        metric_for_best_model="f1",
-        report_to="wandb",
-        # push to hub parameters
-        push_to_hub=cfg.PUSH_TO_HUB,
-        hub_strategy=cfg.HUB_STRATEGY,
-        hub_model_id=f"{cfg.model_name}-{cfg.PROJECT_NAME}",
+            output_dir=cfg.MODEL_DATA_FOLDER,
+            num_train_epochs=cfg.num_epochs,
+            per_device_train_batch_size=cfg.train_batch_size,
+            per_device_eval_batch_size=cfg.eval_batch_size,
+            warmup_steps=cfg.warmup_steps,
+            fp16=cfg.fp16,
+            learning_rate=float(cfg.learning_rate),
+            # logging & evaluation strategies
+            logging_dir=f"{cfg.MODEL_DATA_FOLDER}/logs",
+            logging_steps=50,
+            evaluation_strategy="steps",
+            eval_steps=500,
+            save_strategy="steps",
+            save_steps=500,
+            save_total_limit=2,
+            load_best_model_at_end=True,
+            metric_for_best_model="f1",
+            report_to="wandb",
+            # push to hub parameters
+            push_to_hub=cfg.PUSH_TO_HUB,
+            hub_strategy=cfg.HUB_STRATEGY,
+            hub_model_id=f"{cfg.model_name}-{cfg.PROJECT_NAME}-{cfg.warmup_steps}",
         )
 
         train_dataset, test_dataset = load_data(run, cfg)
@@ -141,12 +196,13 @@ def train(cfg):
 
         # define data_collator
         data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-        
+
         tokenizer = AutoTokenizer.from_pretrained(cfg.model_name)
-        config = AutoConfig.from_pretrained(cfg.model_name, num_labels=number_classes)
 
         model = AutoModelForSequenceClassification.from_pretrained(
-        cfg.model_name, num_labels=number_classes, config=config
+            cfg.model_name,
+            problem_type="single_label_classification",
+            num_labels=number_classes,
         )
 
         trainer = Trainer(
@@ -165,6 +221,7 @@ def train(cfg):
         if cfg.log_model:
             trainer.save_model()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     default_cfg.update(vars(parse_args()))
     train(default_cfg)
